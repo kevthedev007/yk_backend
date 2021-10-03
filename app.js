12a854b04e3a60d19e26e6caece6a9382d87f34f
unknown <kevthedev007@gmail.com>
2021-10-03 19:30:47 +0100

    database setup

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const logger = require('morgan')
+
+if(process.env.NODE_ENV !== 'production') {
+    require('dotenv').config()
+}
+
+const app = express()
+
+//importing routes
+
+
+//Adding middlewares
+app.use(cors())
+app.use(bodyParser.urlencoded({ extended: false}))
+app.use(bodyParser.json())
+
+
+
+const port = process.env.PORT || 3000
+
+app.listen(port, () => {
+    console.log(`server started on port ${port}`)
+})
