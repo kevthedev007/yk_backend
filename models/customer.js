f4bceaf2a0e51de818ea1af16726824225547b21
Kadiri Kelvin <kevthedev007@gmail.com>
2021-10-15 22:48:14 +0100

    Update customer.js

--- models/customer.js
+++ models/customer.js
@@ -1,63 +1,5 @@
 'use strict';
 const {
-<<<<<<< HEAD
-    Model, STRING
-} = require('sequelize');
-module.exports = (sequelize, DataTypes) => {
-    class Customer extends Model {
-        /**
-         * Helper method for defining associations.
-         * This method is not a part of DataTypes lifecycle.
-         * The `models/index` file will call this method automatically.
-         */
-        static associate(models) {
-            // define association here
-        }
-        toJSON(){
-            return{... this.get(), id: undefined}
-        }
-    };
-    Customer.init({
-        uuid: {
-            type: DataTypes.UUID,
-            defaultValue: DataTypes.UUIDV4
-        },
-        full_name: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        email: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            validate: {
-                notNull: {msg: 'user must have an email'},
-                notEmpty: {msg: 'email must not be empty'},
-                isEmail: {msg: 'must be a valid email address'},
-            }
-        },
-        dob: {
-            type: DataTypes.DATE,
-            allowNull: false
-        },
-        phone_no: {
-            type: DataTypes.INTEGER,
-            allowNull: false
-        },
-        status: {
-            type: DataTypes.ENUM("active", "inactive"),
-            defaultValue: "inactive"
-        },
-        last_sigin_date: {
-            type: DataTypes.STRING,
-            defaultValue: "inactive"
-        },
-    }, {
-        sequelize,
-        tableName: 'Customer',
-        modelName: 'Customer',
-    });
-    return Customer;
-=======
   Model
 } = require('sequelize');
 
@@ -109,5 +51,4 @@ module.exports = (sequelize, DataTypes) => {
     modelName: 'Customer',
   });
   return Customer;
->>>>>>> 8f207dba408f5aa0d0cee188faa95762ce6cbe2c
 };
