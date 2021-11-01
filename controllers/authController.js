18003102a0c698d521e34b81e74669fdc114eecd
unknown <kevthedev007@gmail.com>
2021-11-01 14:55:04 +0100

    'response'

--- controllers/authController.js
+++ controllers/authController.js
@@ -18,11 +18,19 @@ const register = async (req, res) => {
     try {
         //check if email exists in database
         const emailExists = await Customer.findOne({ where: { email } });
-        if (emailExists) return res.status(400).send('Email already exists');
+        if (emailExists) return res.status(400).json({
+            sucess: false,
+            code: "400",
+            message: "Email Already Exists"
+        });
 
         //check if phone number exists
         const phoneExists = await Customer.findOne({ where: { phone_no } });
-        if (phoneExists) return res.status(400).send('Phone number already exists');
+        if (phoneExists) return res.status(400).json({
+            sucess: false,
+            code: "400",
+            message: "Phone Number Already Exists"
+        });
 
         //save to customer table
         const newUser = await Customer.create({ full_name, dob, email, phone_no });
@@ -32,10 +40,10 @@ const register = async (req, res) => {
         const now = new Date();
         const expires_in = AddMinutesToDate(now, 10)
 
-        //send otp to mail
+        //json otp to mail
         sendMail(email, otp)
 
-        //send otp to phone
+        //json otp to phone
         sendSMS(otp, phone_no)
 
         //save to otp table
@@ -45,10 +53,21 @@ const register = async (req, res) => {
             expires_in
         })
 
-        return res.status(200).json(newUser.id)
+        return res.status(200).json({
+            success: true,
+            code: "200",
+            message: "Successful",
+            data: {
+                id: newUser.id,
+            }
+        })
 
-    } catch (err) {
-        return res.status(500).send(err)
+    } catch (error) {
+        return res.status(400).json({
+            sucess: false,
+            code: "400",
+            message: error.message
+        })
     }
 }
 
@@ -64,25 +83,43 @@ const verifyOTP = async (req, res) => {
             }
         })
 
-        if (!checkUser) return res.status(400).send('Incorrect OTP')
+        if (!checkUser) return res.status(400).json({
+            sucess: false,
+            code: "400",
+            message: "OTP Incorrect"
+        })
 
         //check if otp has expired
         let currentTime = new Date();
         let otpTime = checkUser.expires_in;
 
-        if (currentTime.getTime() > otpTime.getTime()) return res.status(400).send('OTP expired!')
+        if (currentTime.getTime() > otpTime.getTime()) return res.status(400).json({
+            sucess: false,
+            code: "400",
+            message: "OTP Expired!"
+        })
 
         //change status to true
         checkUser.status = true;
         await checkUser.save()
-        // const updateStatus = await OTP.update({ status: true }, { where: { otp } })
 
         //update customer status
         const status = await Customer.update({ status: 'active' }, { where: { id } })
-        return res.status(200).json(id)
+        return res.status(200).json({
+            success: true,
+            code: "200",
+            message: "Successful",
+            data: {
+                id: id
+            }
+        })
 
-    } catch (err) {
-        return res.status(500).send(err)
+    } catch (error) {
+        return res.status(400).json({
+            sucess: false,
+            code: "400",
+            message: error.message
+        })
     }
 }
 
@@ -92,6 +129,11 @@ const createPIN = async (req, res) => {
     try {
         //to get customer email
         const customer = await Customer.findOne({ where: { id } });
+        if(!customer) return res.status(400).json({
+            sucess: false,
+            code: "400",
+            message: "Customer does not Exist"
+        })
         //save to user model
         const user = await User.create({
             id: id,
@@ -104,9 +146,20 @@ const createPIN = async (req, res) => {
 
         //jwt token
         const token = jwt.sign({ id: user.id }, process.env.SECRET_KEY);
-        return res.status(200).json(token)
-    } catch (err) {
-        return res.status(500).send(err)
+        return res.status(200).json({
+            success: true,
+            code: "200",
+            message: "Successful",
+            data: {
+                token: token
+            }
+        })
+    } catch (error) {
+        return res.status(400).json({
+            sucess: false,
+            code: "400",
+            message: error.message
+        })
     }
 }
 
@@ -116,22 +169,40 @@ const signin = async (req, res) => {
     try {
         //check if email exists
         const userExists = await User.findOne({ where: { email } });
-        if (!userExists) return res.status(400).send('user not registered');
+        if (!userExists) return res.status(400).json({
+            sucess: false,
+            code: "400",
+            message: "User not registered"
+        });
 
         //compare PIN
-        if (PIN !== userExists.PIN) return res.status(400).send('Incorrect PIN')
+        if (PIN !== userExists.PIN) return res.status(400).json({
+            sucess: false,
+            code: "400",
+            message: "Incorrect PIN"
+        })
 
-        //update 
+        //update user status
         userExists.status = true;
         userExists.last_signed_in = Date.now();
         await userExists.save()
-        // const status = await User.update({ status: true }, { where: { email } })
 
         //assign jwt token
         const token = jwt.sign({ id: userExists.id }, process.env.SECRET_KEY);
-        return res.status(200).json(token)
-    } catch (err) {
-        return res.status(500).send(err)
+        return res.status(200).json({
+            success: true,
+            code: "200",
+            message: "Successful",
+            data: {
+                token: token
+            }
+        })
+    } catch (error) {
+        return res.status(400).json({
+            sucess: false,
+            code: "400",
+            message: error.message
+        })
     }
 }
 
@@ -141,14 +212,18 @@ const forgetPIN = async (req, res) => {
 
     try {
         const user = await User.findOne({ where: { email } })
-        if (!user) return res.status(400).send('customer does not exist')
+        if (!user) return res.status(400).json({
+            sucess: false,
+            code: "400",
+            message: "Email does not Exist!"
+        })
 
         //Generate OTP
         const otp = otpGenerator.generate(4, { alphabets: false, specialChars: false, upperCase: false });
         const now = new Date();
         const expires_in = AddMinutesToDate(now, 10)
 
-        //send to mail
+        //json to mail
         sendMail(email, otp)
 
         //save to otp table
@@ -158,9 +233,17 @@ const forgetPIN = async (req, res) => {
             expires_in
         })
 
-        return res.status(200).send('password reset pin has been sent to your mail')
+        return res.status(200).json({
+            success: true,
+            code: "200",
+            message: 'Password reset pin has been sent to your mail'
+        })
     } catch (err) {
-        return res.status(500).send(err)
+        return res.status(400).json({
+            sucess: false,
+            code: "400",
+            message: error.message
+        })
     }
 }
 
@@ -169,7 +252,11 @@ const resetCode = async (req, res) => {
 
     //check user
     const user = await User.findOne({ where: { id }})
-    if(!user) return res.status(400).send('user not found!')
+    if(!user) return res.status(400).json({
+        success: false,
+        code: "400",
+        message: 'user not found!'
+    })
 
     try {
         const checkOTP = await OTP.findOne({
@@ -183,9 +270,18 @@ const resetCode = async (req, res) => {
         checkOTP.status = true;
         await checkOTP.save()
 
-        return res.status(200).send('reset code correct')
+        return res.status(200).json({
+            sucess: true,
+            code: "200",
+            message: "Reset Code Correct!"
+        })
+
     } catch (err) {
-        return res.status(500).send(err)
+        return res.status(400).json({
+            sucess: false,
+            code: "400",
+            message: error.message
+        })
     }
 }
 
@@ -195,7 +291,11 @@ const resetPIN = async (req, res) => {
     try {
         //check user
         const user = await User.findOne({ where: { id }})
-        if(!user) return res.status(400).send('user not found!')
+        if(!user) return res.status(400).json({
+            success: true,
+            code: "400",
+            message: 'user not found!'
+        })
 
         //change PIN
         user.PIN = PIN;
@@ -207,9 +307,17 @@ const resetPIN = async (req, res) => {
             activity: 'pin_reset'
         })
 
-        res.json("PIN changed successfully")
-    } catch (err) {
-        return res.status(500).send(err)
+        return res.json({
+            success: true,
+            code: "200",
+            message: 'PIN changed successfully'
+        })
+    } catch (error) {
+        return res.status(500).json({
+            success: true,
+            code: "400",
+            message: error.message
+        })
     }
 }
 
